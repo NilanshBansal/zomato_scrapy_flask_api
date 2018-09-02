ca598767497b27ab5ac9f44c2c0b76a23cf40876
Nilansh Bansal <bansalnilansh@gmail.com>
2018-09-02 23:25:28 +0530

    hosting

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask,jsonify,request,render_template
+from flask import Flask,request,render_template
 from allReviews import AllreviewsSpider
 import json
 from scrapy.crawler import CrawlerRunner
 import time
 
-app = Flask('Scrape With Flask')
+app = Flask(__name__)
 crawl_runner = CrawlerRunner()      # requires the Twisted reactor to run
 
 reviews_list = []                    # store quotes
